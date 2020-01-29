6ce8c4293332023d28bfaa3f2b1343f1fb27612c
My Name is <hugo.tiaiba@eemi.com>
2020-01-29 11:48:06 +0100

    Voici le commit numéro 2 contenant la v 1.1.

---
+++ README.md
@@ -0,0 +1,1 @@
+Ce programme Node.js démarre un serveur HTTP qui va répondre "Hello World" à chaque requête GET.
--- server.js
+++ server.js
@@ -1,2 +1,10 @@
-console.log ('Hello World');
+onst express = require('express')
+const app = express()
 
+app.get('/', function (req, res) {
+	res.send('Hello World')
+	})
+
+app.listen(3000, function ()	{
+	console.log('Example app listening on port 3000!')
+	})
